455f4a0422432469888b5d32a45c502934d17e91
Jana <yanix2x2@gmail.com>
2025-06-16 20:53:09 +0300

    add evolutions

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -8,6 +8,20 @@ class Pokemon(models.Model):
     title_ru = models.CharField(max_length=200, null=True, blank=True)
     title_en = models.CharField(max_length=200, null=True, blank=True)
     title_jp = models.CharField(max_length=200, null=True, blank=True)
+    evolution_from = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='evolutions_from'
+    )
+    evolution_to = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='evolutions_to'
+    )
 
     def __str__(self):
         return self.title
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -35,25 +35,19 @@ def show_all_pokemons(request):
     for pokemon in Pokemon.objects.all():
         for pokemon_entity in PokemonEntity.objects.all():
             moscow_time = timezone.localtime(timezone.now())
-            if pokemon_entity.appeared_at < moscow_time < pokemon_entity.disappeared_at:
-                img_url = pokemon.image.url if pokemon.image else None
-                if img_url:
-                    img_url = request.build_absolute_uri(img_url)
-                    add_pokemon(
-                        folium_map, 
-                        pokemon_entity.lat,
-                        pokemon_entity.lon,
-                        img_url
-                    )
+            if pokemon_entity.appeared_at < moscow_time < pokemon_entity.disappeared_at: 
+                add_pokemon(
+                    folium_map, 
+                    pokemon_entity.lat,
+                    pokemon_entity.lon,
+                    get_image_url(request, pokemon)
+                )
 
     pokemons_on_page = []
     for pokemon in Pokemon.objects.all():
-        img_url = pokemon.image.url if pokemon.image else None
-        if img_url:
-            img_url = request.build_absolute_uri(img_url)
         pokemons_on_page.append({
             'pokemon_id': pokemon.id,
-            'img_url': img_url,
+            'img_url': get_image_url(request, pokemon),
             'title_ru': pokemon.title,
         })
 
@@ -71,29 +65,46 @@ def show_pokemon(request, pokemon_id):
 
     pokemon_entities = PokemonEntity.objects.filter(pokemon=pokemon)
 
-    img_url = pokemon.image.url if pokemon.image else None
-    if img_url:
-        img_url = request.build_absolute_uri(img_url)
-
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon_entity in pokemon_entities:   
         add_pokemon(
             folium_map, 
             pokemon_entity.lat,
             pokemon_entity.lon,
-            img_url
+            get_image_url(request, pokemon)
         )
 
     pokemon_on_page = {
         'pokemon_id': pokemon.id,
-        'img_url': img_url,
+        'img_url': get_image_url(request, pokemon),
         'title_ru': pokemon.title,
         'title_jp': pokemon.title_jp,
         'title_en': pokemon.title_en,
-        'description': pokemon.description
+        'description': pokemon.description,
     }
+
+    if pokemon.evolution_from:
+        pokemon_on_page['previous_evolution'] = {
+            'title_ru': pokemon.evolution_from.title,
+            'pokemon_id': pokemon.evolution_from.id,
+            'img_url': get_image_url(request, pokemon.evolution_from),
+        }    
+
+    if pokemon.evolution_to:
+        pokemon_on_page['next_evolution'] = {
+            'title_ru': pokemon.evolution_to.title,
+            'pokemon_id': pokemon.evolution_to.id,
+            'img_url': get_image_url(request, pokemon.evolution_to)
+        }  
 
     return render(request, 'pokemon.html', context={
         'map': folium_map._repr_html_(), 
         'pokemon': pokemon_on_page
     })
+
+
+def get_image_url(request, pokemon):
+    img_url = pokemon.image.url if pokemon.image else None
+    if img_url:
+        img_url = request.build_absolute_uri(img_url)
+    return img_url
